5fe33fe4a02a3d3d0bc99ae93c35e1adb793408f
Julian Jelfs <julian.jelfs@gmail.com>
2021-12-03 12:59:26 +0000

    log port

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const server = app.listen(port, () => {
   if (process.send) {
     process.send(`Server running at http://localhost:${port}\n\n`);
   }
+  console.log(`Server running at http://localhost:${port}\n\n`);
 });
 
 websockets.init(server);
